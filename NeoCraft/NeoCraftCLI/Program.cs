c54875e6a7f188aa4f6253f1624e566d82256344
NeonStudios-dev <info@neonstudios.dev>
2025-04-21 18:46:21 +0200

    Uploaded everything to GitHub

---
+++ NeoCraft/NeoCraftCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeoCraftCLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Look Everything Is beeing remade! :D");
+        }
+    }
+}
