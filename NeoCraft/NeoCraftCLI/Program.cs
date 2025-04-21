517a550989b083f2d213ac840be733dc8f22f8ec
NeonStudios-dev <info@neonstudios.dev>
2025-04-21 19:13:35 +0200

    First Release On the CLI platform! :D

--- NeoCraft/NeoCraftCLI/Program.cs
+++ NeoCraft/NeoCraftCLI/Program.cs
@@ -6,8 +6,38 @@ namespace NeoCraftCLI
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Look Everything Is beeing remade! :D");
-            Console.WriteLine("Test");
-        }
+            // Check if the user provided a command-line argument
+if (args.Length > 0)
+{
+    // Print the first argument to the console
+    Console.WriteLine($"Argument: {args[0]}");
+    
+    switch (args[0])
+    {
+        case "--help":
+            // Print help information
+            Console.WriteLine("Usage: NeoCraftCLI [options]");
+            Console.WriteLine("Options:");
+            Console.WriteLine("  --help       Show this help message");
+            Console.WriteLine("  --version    Show the version of the application");
+            break;
+            
+        case "--version":
+            // Print the version of the application
+            Console.WriteLine("NeoCraftCLI Version alpha preview 0.0.1");
+            break;
+            
+        default:
+            MainMenu();
+            break;
     }
 }
+        }
+
+ public static void MainMenu(){
+        
+    }
+    }
+
+}
+
--- NeoCraft/NeoCraftCLI/obj/Debug/net9.0/NeoCraftCLI.AssemblyInfo.cs
+++ NeoCraft/NeoCraftCLI/obj/Debug/net9.0/NeoCraftCLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeoCraftCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54875e6a7f188aa4f6253f1624e566d82256344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cec57316e0a3e13c786a66b55ffe9a064141bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeoCraftCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeoCraftCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
